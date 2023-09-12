1205a7d9cfb41d090dcc48c8ef2180f572e22d78
shanu4721926 <shanu@inzint.com>
2023-09-12 15:48:17 +0530

    feat: user Api Created

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,33 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import mongoose, { ConnectOptions } from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/user";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
-// Define routes here
 
-const port = process.env.PORT || 5000;
+
+// MongoDB connection
+const mongoUrl =
+  "mongodb+srv://amitsarraf:S%40m12345@flat-service.qartbfp.mongodb.net/vidychat?authSource=admin&replicaSet=atlas-73o9j1-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true";
+
+  mongoose.connect(mongoUrl as string);
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection failed'));
+  db.once('open', async () => {
+    console.log('Database conencted successfully!');
+  });
+
+// Routes
+app.use("/auth", userRoutes);
+
+// Start the server
+const port = process.env.PORT || 4200;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ src/controllers/userController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User, { IUser } from "../models/userModal";
+
+const secretKey = "your-secret-key"; // Replace with your secret key
+const expiresIn = "1h"; // Token expiration time
+
+class UserController {
+  static async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      // Find the user by email
+      const user: IUser | null = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid password" });
+      }
+
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn });
+
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error("Login error:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+export default UserController;
